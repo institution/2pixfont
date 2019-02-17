5cdf11f2f583712e631b992d5858fc1741b69929
Staszek R <sta256+git@gmail.com>
2019-02-18 00:11:46 +0100

    mono

--- src/pixfont/main.cpp
+++ src/pixfont/main.cpp
@@ -46,11 +46,51 @@ struct Buffer2D
 };
 
 
-
-void copy(Buffer2D & trg, v2i const& pos, FT_Bitmap const& bm)
+void copy_mono(Buffer2D & trg, v2i const& pos, FT_Bitmap const& bm)
 {
-	if (int(bm.pixel_mode) != 2) fail("required pixel_mode 2");
+	if (int(bm.pixel_mode) != FT_PIXEL_MODE_MONO) fail("required FT_PIXEL_MODE_MONO");
 	if (int(bm.palette_mode) != 0) fail("required palette_mode 0");
+
+	uint8_t * line_p = bm.buffer;
+	uint8_t * byte_p = nullptr;
+
+	for (size_t j = 0; j < bm.rows; ++j) {
+
+		byte_p = line_p;
+
+		size_t i = 0;
+		while (i < bm.width) {
+
+			uint8_t byte = *byte_p;
+
+			for (size_t k = 0; k < 8 and i < bm.width; ++k)
+			{
+				bool mono = byte & 0B10000000;
+
+				auto tp = 4 * ((pos[0] + i) + (pos[1] + j) * trg.dim[0]);
+				trg.buf.at(tp+0) = 0;
+				trg.buf.at(tp+1) = 0;
+				trg.buf.at(tp+2) = 0;
+				trg.buf.at(tp+3) = mono * 255;
+
+				++i;
+				byte = byte << 1;
+			}
+
+			byte_p += 1;
+		}
+
+		line_p += bm.pitch;
+	}
+
+}
+
+
+void copy_gray(Buffer2D & trg, v2i const& pos, FT_Bitmap const& bm)
+{
+	if (int(bm.pixel_mode) != FT_PIXEL_MODE_GRAY) fail("required FT_PIXEL_MODE_GRAY");
+	if (int(bm.palette_mode) != 0) fail("required palette_mode 0");
+
 
 	uint8_t * bp = bm.buffer;
 	for (size_t j=0; j<bm.rows; ++j) {
@@ -124,52 +164,39 @@ void fill(Buffer2D & trg, v2i const& pos, v2i const dim, Color4 const color) {
 
 int main(int argc, char * argv[])
 {
-	assert(argc == 3);
+	int verbose = 1;
+
+	assert(argc == 2);
 	// argv[1] -- font path
-	// argv[2] -- font size
-
-
-
-	int pixsize = atoi(argv[2]);
-
-	std::cout << "INFO: pixsize " << pixsize << "\n";
 
 	auto fe = filesys::filename(argv[1]);
 	auto fn = fe.substr(0, fe.size()-4);
 
-
+	int pixsize = 128;
+	std::cout << "INFO: pixsize " << pixsize << "\n";
 
 	FT_Library library;
-
 	FT_CALL(FT_Init_FreeType, &library);
-
 	FT_Face face;
-
 	FT_CALL(FT_New_Face,
 		library,
 		argv[1], //"LiberationSans-Regular.ttf",
-		//"ColTiny.ttf",
 		0,
 		&face
 	);
 
-
-
-
-	string fontname = format("%||-%||-%||", face->family_name, face->style_name, pixsize);
+	string fontname = format("%||-%||", face->family_name, face->style_name);
 	string rc_name = format("%||.rc", fontname);
 	string png_name = format("%||.png", fontname);
 
 	std::ofstream rc_file(rc_name);
 
-
-	if (0) {
+	if (verbose > 1) {
 		print("num_faces %||\n", face->num_faces);
 		print("num_glyphs %||\n", face->num_glyphs);
 		print("face_flags %||\n", face->face_flags);
 		print("units_per_EM %||\n", face->units_per_EM);
 		print("num_fixed_sizes %||\n", face->num_fixed_sizes);
-
 		print("available_sizes");
 		for (int i = 0; i < face->num_fixed_sizes; ++i)
 		{
@@ -181,13 +208,9 @@ int main(int argc, char * argv[])
 
     /*
     1/72th of an inch
-
     DPI dot/inch * 16 1/72 inch
-
     16 * DPI/72 dot
     */
-
-
 	FT_CALL(FT_Set_Char_Size,
 		face,
 		0,          // char_width in 1/64th of points
@@ -196,19 +219,19 @@ int main(int argc, char * argv[])
 		72          // y dpi
 	);
 
-
-
-	auto& m = face->size->metrics;
-	print("ascender %||\n", m.ascender >> 6);
-	print("descender %||\n", m.descender >> 6);
-	print("height %||\n", m.height >> 6);
-
+	auto & m = face->size->metrics;
+	if (verbose > 0) {
+		print("ascender %||\n", m.ascender >> 6);
+		print("descender %||\n", m.descender >> 6);
+		print("height %||\n", m.height >> 6);
+	}
 
 	auto f_asc = m.ascender >> 6;
 	auto f_desc = m.descender >> 6;
 	auto f_height = m.height >> 6;
 	auto f_width = f_height / 2;
 
+	print(rc_file, "font _\n");
 	print(rc_file, "name \"%||\"\n", fontname);
 	print(rc_file, "max_sprites 256\n");
 	print(rc_file, "height %||\n", f_height);
@@ -218,13 +241,11 @@ int main(int argc, char * argv[])
 	print(rc_file, "pad 2 0\n");
 	print(rc_file, "\n");
 
-
 	/*FT_CALL(FT_Set_Pixel_Sizes,
 		face,
 		0,      // pix width
 		16      // pix height
 	);*/
-
 
     auto chars = std::string(
 		" !\"#$%&'()*+,-./"
@@ -235,52 +256,46 @@ int main(int argc, char * argv[])
 		"abcdefghijklmnopqrstuvwxyz"
 		"{|}~"
 	);
-    /*auto chars = std::string(
-		"abcd"
-	);
-    */
 
+	std::ofstream fdesc(format("%||.csv", fontname));
+	print(fdesc, "code,bearing_x,bearing_y,advance,width,height,pos_x,pos_y\n");
 
-    std::ofstream fdesc(format("%||.csv", fontname));
-    print(fdesc, "code,bearing_x,bearing_y,advance,width,height,pos_x,pos_y\n");
+	auto xx = (chars.size()/8) * pixsize;
+	auto yy = pixsize * 10;
 
+	print("img.dim %|| %||\n", xx, yy);
 
-    Buffer2D image(v2i(pixsize * f_height, pixsize * f_height));
+    Buffer2D image(v2i(xx,yy));
 	fill(image, v2i(0,0), image.dim, Color4(100,100,100,255));
 
     v2i sep(4,4);
     v2i pos = sep;
 
-	ext::darray2<uint8_t, int16_t> img;
-	img.resize(v2s(pixsize * f_height, pixsize * f_height), 0);
 
-	//FontHead fh;
-	//fh.height = f_height;
-	//fh.ascender = (m.ascender >> 6);
-	//fh.descender = (m.descender >> 6);
+	//ext::darray2<uint8_t, int16_t> img;
+	//img.resize(v2s(xx,yy), 0);
 
-	//write(ftab, fh);
 
-    //write(ftab, (uint32_t)chars.size());
-    for (auto charcode: chars)
+
+
+
+	for (auto charcode: chars)
     {
+		print("rendering %||\n", charcode);
+
 		auto glyph_index = FT_Get_Char_Index(face, charcode);
-
-
 		FT_CALL(FT_Load_Glyph, face, glyph_index, FT_LOAD_DEFAULT);
+		//FT_CALL(FT_Render_Glyph, face->glyph, FT_RENDER_MODE_NORMAL);
+		FT_CALL(FT_Render_Glyph, face->glyph, FT_RENDER_MODE_MONO);
 
-		FT_CALL(FT_Render_Glyph, face->glyph, FT_RENDER_MODE_NORMAL);
+
 
 		auto gly = face->glyph;
-		auto *met = &gly->metrics;
-
+		auto * met = &gly->metrics;
 		auto & bm = gly->bitmap;
-
 
 		//unsigned char*  buffer;
 		//void*           palette;
-
-
 /*
 		print("%||\n", charcode);
 		print(" width %||\n", met->width);
@@ -322,11 +337,10 @@ int main(int argc, char * argv[])
 
 		auto gly_pos = pos + v2i(gly->bitmap_left, f_asc - gly->bitmap_top);
 
-		copy(image, gly_pos, bm);
-		copy_bitmap(img, v2s(gly_pos[0], gly_pos[1]), bm);
+		copy_mono(image, gly_pos, bm);
+		//copy_bitmap(img, v2s(gly_pos[0], gly_pos[1]), bm);
 
 		pos[0] += adv + sep[0];
-
 
 		print(fdesc, "%||,%||,%||,%||,%||,%||,%||,%||\n",
 			int(charcode),
@@ -338,18 +352,6 @@ int main(int argc, char * argv[])
 			gly_pos[0],
 			gly_pos[1]
 		);
-
-		/*
-		GlyphHead g;
-		g.code = charcode;
-		g.bearing_x = gly->bitmap_left;
-		g.bearing_y = gly->bitmap_top;
-		g.advance = (gly->advance.x >> 6);
-		g.width = bm.width;
-		g.height = bm.rows;
-		g.pos_x = gly_pos[0];
-		g.pos_y = gly_pos[1];
-		*/
 
 		print(rc_file, "sprite %||\n", int(charcode));
 
@@ -364,7 +366,6 @@ int main(int argc, char * argv[])
 		print(rc_file, "delta %|| %||\n", gly->bitmap_left, -gly->bitmap_top + f_asc);
 		print(rc_file, "adv %||\n", (gly->advance.x >> 6));
 		print(rc_file, "\n");
-
 	}
 
 	rc_file.close();
@@ -375,7 +376,6 @@ int main(int argc, char * argv[])
 		image.dim[0],
 		image.dim[1]
 	);
-
 
 	return 0;
 }
